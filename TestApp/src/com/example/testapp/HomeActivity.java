76fe54c1de89d371c81ef593164c9bffc3457139
unknown <volgin@clarksville.mercury.office>
2014-10-06 16:02:14 +0400

    HomeActivity changes

--- TestApp/src/com/example/testapp/HomeActivity.java
+++ TestApp/src/com/example/testapp/HomeActivity.java
@@ -1,16 +1,85 @@
 package com.example.testapp;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class HomeActivity extends Activity {
 
+	private static final String CURRENT_MUSIC_DOWNLOADING_PROGRESS_KEY = "currentProgress";
+	private static final int MAX_PROGRESS = 10000;
+
+	ProgressDialog musicDownloadingProgressDialog;
+	private Thread dialogUpdatingThread;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+
+		int currentProgress = getSavedProgress(savedInstanceState);
+		if (currentProgress < MAX_PROGRESS) {
+			musicDownloadingProgressDialog = new ProgressDialog(this);
+			initializeProgressDialog(musicDownloadingProgressDialog,
+					currentProgress);
+		}
+	}
+
+	private void initializeProgressDialog(final ProgressDialog pd,
+			int currentProgress) {
+		pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+
+		pd.setTitle("Title");
+		pd.setMessage("Message");
+		pd.setMax(MAX_PROGRESS);
+
+		pd.show();
+		pd.setProgress(currentProgress);
+
+		dialogUpdatingThread = new Thread() {
+			@Override
+			public void run() {
+				while (pd.getProgress() < MAX_PROGRESS
+						&& !Thread.interrupted()) {
+					try {sleep(50);} catch (InterruptedException e) {}
+					pd.incrementProgressBy(100);
+				}
+				pd.dismiss();
+			}
+		};
+		dialogUpdatingThread.start();
+	}
+
+	private int getSavedProgress(Bundle savedInstanceState) {
+		int progress = savedInstanceState != null ? savedInstanceState
+				.getInt(CURRENT_MUSIC_DOWNLOADING_PROGRESS_KEY) : 0;
+
+		Log.d(ACTIVITY_SERVICE, "progress get: " + progress);
+		return progress;
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		int downloadingProgress = musicDownloadingProgressDialog != null ? musicDownloadingProgressDialog
+				.getProgress() : MAX_PROGRESS;
+
+		outState.putInt(CURRENT_MUSIC_DOWNLOADING_PROGRESS_KEY, downloadingProgress);
+		Log.d(ACTIVITY_SERVICE, "progress saved: " + downloadingProgress);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (dialogUpdatingThread != null) {
+			dialogUpdatingThread.interrupt();
+		}
+		if (musicDownloadingProgressDialog != null) {
+			musicDownloadingProgressDialog.dismiss();
+		}
 	}
 
 	@Override
