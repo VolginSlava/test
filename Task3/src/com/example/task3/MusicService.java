16926ab0af5867c7606fa340ad030f5e14c5dc38
unknown <volgin@clarksville.mercury.office>
2014-10-13 16:08:55 +0400

    MusicService implemented

--- Task3/src/com/example/task3/MusicService.java
+++ Task3/src/com/example/task3/MusicService.java
@@ -2,43 +2,135 @@ package com.example.task3;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnErrorListener;
 import android.media.MediaPlayer.OnPreparedListener;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MusicService extends Service implements OnPreparedListener,
-		OnErrorListener, OnCompletionListener {
+		OnCompletionListener, OnErrorListener {
 
+	private static final String MUSIC_SERVICE = "MusicService";
+
+	public class MusicBinder extends Binder {
+
+		public MusicService getService() {
+			return MusicService.this;
+		}
+	}
+
+	private final IBinder musicBind = new MusicBinder();
+	private byte[] fileBytes;
 	private MediaPlayer player;
-
-	// public class
+	private boolean prepared = false;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+
 		player = new MediaPlayer();
-
-		setListeners();
+		addPlayerListeners();
 	}
 
-	private void setListeners() {
+	private void addPlayerListeners() {
 		player.setOnPreparedListener(this);
-		player.setOnCompletionListener(this);
 		player.setOnErrorListener(this);
+		player.setOnCompletionListener(this);
+	}
+
+	public void setMusic(byte[] bytes) {
+		fileBytes = bytes;
+		prepared = false;
+		player.reset();
+
+		File temp = null;
+		try {
+			temp = saveAsTempFile("music", "dat", fileBytes);
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			Log.e(MUSIC_SERVICE, "Can't write data to temp file.");
+			throw new RuntimeException(e);
+		}
+
+		FileInputStream in = null;
+		try {
+			in = new FileInputStream(temp);
+			player.setDataSource(in.getFD());
+			player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		} catch (IllegalArgumentException e) {
+			Log.e(MUSIC_SERVICE, "", e);
+		} catch (IllegalStateException e) {
+			Log.e(MUSIC_SERVICE,
+					"Exception while trying to set source data to mediaPlayer in incorrect state.",
+					e);
+		} catch (IOException e) {
+			Log.e(MUSIC_SERVICE, "Can't read data from temp file.");
+			throw new RuntimeException(e);
+		} finally {
+			closeFinally(in);
+		}
+	}
+
+	private File saveAsTempFile(String prefix, String suffix, byte[] bytes)
+			throws IOException, FileNotFoundException {
+		File temp = File.createTempFile(prefix, suffix, getCacheDir());
+		FileOutputStream out = new FileOutputStream(temp);
+		try {
+			out.write(bytes);
+		} finally {
+			closeFinally(out);
+		}
+		return temp;
+	}
+
+	public void playMusic() {
+		if (!prepared) {
+			player.prepareAsync();
+		} else {
+			resumeMusic();
+		}
+	}
+
+	public void pauseMusic() {
+		player.pause();
+	}
+
+	private void resumeMusic() {
+		player.start();
+	}
+
+	public boolean isPlaying() {
+		return player.isPlaying();
 	}
 
 	@Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public IBinder onBind(Intent intent) {
+		return musicBind;
 	}
 
 	@Override
-	public void onPrepared(MediaPlayer mp) {
-		// TODO Auto-generated method stub
+	public boolean onUnbind(Intent intent) {
+		player.stop();
+		player.release();
+		return false;
+	}
 
+	@Override
+	public boolean onError(MediaPlayer mp, int what, int extra) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
@@ -48,9 +140,19 @@ public class MusicService extends Service implements OnPreparedListener,
 	}
 
 	@Override
-	public boolean onError(MediaPlayer mp, int what, int extra) {
-		// TODO Auto-generated method stub
-		return false;
+	public void onPrepared(MediaPlayer player) {
+		prepared = true;
+		resumeMusic();
 	}
 
+	private void closeFinally(Closeable c) {
+		try {
+			if (c != null) {
+				c.close();
+	}
+		} catch (IOException e) {
+			Log.i(MUSIC_SERVICE,
+					"Exception occures while trying to close resource", e);
+		}
+	}
 }
